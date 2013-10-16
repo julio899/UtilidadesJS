f6ab5d5f0b1e2dbad45ba0ffb1cb4b2d7f3fd60b
julio899 <julio899@gmail.com>
2013-10-16 11:57:46 -0430

    add function mostrar propiedades

--- funcines.js
+++ funcines.js
@@ -8,3 +8,11 @@ function sleep(milliseconds) {
 }
 /*Fin  de la Funcion sleep 
 con ella podemos implementar igual que en Java*/
+
+function mostrar_propiedades(obj, objName) { 
+   var result = ""; 
+   for (var i in obj) { 
+      result += objName + "." + i + " = " + obj[i] + "\n"; 
+   } 
+   return result; 
+}/* fin de la funcion Mostrar Propiedades de un Objeto*/
